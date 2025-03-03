333c0f194d746c921c05cd9c962b90116d16a81f
Kwanghoon Choi <lazyswamp@gmail.com>
2025-03-03 11:49:19 +0900

    add containers to package.yaml; fix Tokens.hs and; create Lexer.hs

---
+++ labs/ArithCompiler/app/Lexer.hs
@@ -0,0 +1,38 @@
+module Lexer(lexerSpec) where
+
+import CommonParserUtil
+import Token 
+
+import qualified Data.Map as Map
+
+-- No parser state is used
+type ParserState = ()
+
+mkFn :: Token -> LexAction Token IO ParserState
+mkFn tok = \_ -> return $ Just tok
+
+skip :: LexAction Token IO ParserState
+skip = \_ -> return $ Nothing
+
+lexerSpec :: LexerSpec Token IO ParserState
+lexerSpec = LexerSpec
+  {
+    endOfToken    = END_OF_TOKEN,
+    lexerSpecList = 
+      [ 
+        ("[ \t\n\r]" , skip)    -- space+ where space = [' ' '\t' '\n' '\r']
+
+        -- Typical definition of keywords or identifiers
+        , ("[_a-zA-Z][_a-zA-Z0-9]*", keywordOrIdentifier)
+      ]
+  }
+
+keywordMap :: Map.Map String Token
+keywordMap = Map.fromList (map swap keywords)
+  where swap (a,b) = (b,a)
+
+keywordOrIdentifier :: Monad m => String -> m (Maybe Token)
+keywordOrIdentifier text = 
+  case Map.lookup text keywordMap of
+    Nothing -> return $ Just IDENTIFIER
+    Just tok -> return $ Just tok
--- labs/ArithCompiler/app/Token.hs
+++ labs/ArithCompiler/app/Token.hs
@@ -1,17 +1,25 @@
-module Token(Token(..)) where
+module Token(Token(..), keywords, fromToken) where
 
 import TokenInterface
 
 data Token = END_OF_TOKEN
-  {- List token names here -}
+  {- List token names here. -}
+  | IDENTIFIER
   deriving (Eq, Show)
 
 tokenStrList :: [(Token,String)]
 tokenStrList =
   [ (END_OF_TOKEN, "$")  -- 
     {- List pairs of a token name 
-       and a textual representation here -}  
+       and a textual representation here. -}
+    , (IDENTIFIER, "identifier")
   ]
+
+keywords :: [(Token, String)]
+keywords =
+  [ {- List pairs of a keyword token name
+       and a textual representation here. -}
+  ]  
 
 findTok :: Token -> [(Token, String)] -> Maybe String
 findTok _tok [] = Nothing
