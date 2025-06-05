3d79f1368a30c3a1b322a1f0199367065995adfc
Kwanghoon Choi <lazyswamp@gmail.com>
2025-06-05 22:24:05 +0900

    Expr

--- labs/MiniCinHs/app/ast/Expr.hs
+++ labs/MiniCinHs/app/ast/Expr.hs
@@ -1,4 +1,7 @@
 module Expr where
+import Distribution.Simple.Configure (ConfigStateFileError(ConfigStateFileNoHeader))
+import Data.Type.Bool (If)
+import Distribution.Simple.Program.GHC (GhcProfAuto(GhcProfAutoExported))
 
 data Parse =
     ParseSeq  { fromParseSeq  :: [Expr] } -- Expr Sequence: Expr1; ... ; Exprn
@@ -13,36 +16,63 @@ toParseSeq exprs = ParseSeq exprs
 toParseExpr :: Expr -> Parse
 toParseExpr expr = ParseExpr expr
 
-data Expr =
-    Lit { fromLit :: Int }
-  | Var { fromVar :: String }
-  | BinOp { kindFromBinOp :: BinOpKind,
-            leftOpFromBinOp :: Expr,
-            rightOpFromBinOp :: Expr }
-  | Assign { lhsFromAssign :: String,
-             rhsFromAssign :: Expr  }
+-- data Expr =
+--     Program
+--   | FuncDef 
+--   | ConstNode 
+--   | IntNode 
+--   | VoidNode 
+--   | FormalParam
+--   | ParamDecl
+--   | CompoundStmt 
+--   | DeclList 
+--   | Decl 
+--   | DeclItem 
+--   | SimpleVar 
+--   | ArrayVar 
+--   | StmtList 
+--   | ExprStmt 
+--   | IfStmt 
+--   | WhileStmt 
+--   | ReturnStmt 
+--   | AssignExpr
+--   | AddAssign 
+--   | SubAssign 
+--   | MulAssign 
+--   | DivAssign 
+--   | ModAssign 
+--   | LogicalOr
+--   | LogicalAnd
+--   | EqExpr
+--   | NeqExpr 
+--   | GTExpr 
+--   | LTExpr 
+--   | GEExpr 
+--   | AddExpr 
+--   | SubExpr 
+--   | MulExpr 
+--   | DivExpr 
+--   | ModExpr 
+--   | UnaryMinusExpr 
+--   | LogicalNotExpr 
+--   | PreIncExpr 
+--   | PreDecExpr
+--   | PostIncExpr 
+--   | PostDecExpr
+--   | IndexExpr 
+--   | CallExpr
+--   | ActualParam
+--   deriving (Show, Eq)
 
-data BinOpKind = ADD | SUB | MUL | DIV
+-- data Expr
+-- type ExprList = [Expr]
+-- data Stmt 
+-- type StmtList = [Stmt]
+-- data Decl 
+-- type DeclList = [Decl]
+-- data ParamDecl 
+-- type ParamDeclList = [ParamDecl]
+-- data ExternDecl 
+-- type ExternDeclList = [ExternDecl]
+-- type TranslationUnit = ExternDeclList
 
-pprintParse :: Parse -> String
-pprintParse (ParseSeq exprs) =
-  let insSemicolon []         = ""
-      insSemicolon [str]      = str
-      insSemicolon (str:strs) = str ++ "; " ++ insSemicolon strs
-  in insSemicolon (map pprint exprs)
-    
-pprintParse (ParseExpr expr) = pprint expr
-
-pprint :: Expr -> String
-pprint (Lit i) = show i
-pprint (Var v) = v
-pprint (BinOp Expr.ADD left right) =
-  "(" ++ pprint left ++ " + " ++ pprint right ++ ")"
-pprint (BinOp Expr.SUB left right) =
-  "(" ++ pprint left ++ " - " ++ pprint right ++ ")"
-pprint (BinOp Expr.MUL left right) =
-  "(" ++ pprint left ++ " * " ++ pprint right ++ ")"
-pprint (BinOp Expr.DIV left right) =
-  "(" ++ pprint left ++ " / " ++ pprint right ++ ")"
-pprint (Assign x expr) =   
-  "(" ++ x ++ " = " ++ pprint expr ++ ")"
