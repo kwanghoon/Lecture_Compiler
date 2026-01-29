3b4bd83b60acb8771e071c1c409a26972ae85e4a
Kwanghoon Choi <lazyswamp@gmail.com>
2026-01-29 19:57:34 +0900

    커맨드 인자

--- Andrew/lab/chap4/handcrafted/Main.java
+++ Andrew/lab/chap4/handcrafted/Main.java
@@ -3,12 +3,20 @@ import visitor.*;
 
 public class Main {
    public static void main(String [] args) {
-      try {
-         Program root = new MiniJavaParser(System.in).Goal();
-          root.accept(new PrettyPrintVisitor());
-      }
-      catch (ParseException e) {
-         System.out.println(e.toString());
+      for (String path : args) {
+         try {
+            java.io.InputStream in = new java.io.FileInputStream(path);
+            MiniJavaParser parser = new MiniJavaParser(in);
+            Program root = parser.Goal();
+            root.accept(new PrettyPrintVisitor());
+            System.out.println("Parse succeeded: " + path);
+         } catch (ParseException e) {
+            System.err.println("Parse error in " + path + ":\n" + e.toString());
+            System.exit(2);
+         } catch (java.io.IOException e) {
+            System.err.println("I/O error for " + path + ":\n" + e.toString());
+            System.exit(3);
+         }
       }
    }
 }
