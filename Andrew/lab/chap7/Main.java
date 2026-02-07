475b373b38e02505c57a43413754805dae407610
Kwanghoon Choi <lazyswamp@gmail.com>
2026-02-07 18:30:57 +0900

    chap5의 파서, 타입체커, 파스 트리를 chap7로 복사

---
+++ Andrew/lab/chap7/Main.java
@@ -0,0 +1,22 @@
+import syntaxtree.*;
+import visitor.*;
+
+public class Main {
+   public static void main(String [] args) {
+      for (String path : args) {
+         try {
+            java.io.InputStream in = new java.io.FileInputStream(path);
+            MiniJavaParser parser = new MiniJavaParser(in);
+            Program root = parser.Goal();
+            root.accept(new PrettyPrintVisitor());
+            new TypeCheckVisitor().check(root);
+         } catch (ParseException e) {
+            System.err.println("Parse error in " + path + ":\n" + e.toString());
+            System.exit(2);
+         } catch (java.io.IOException e) {
+            System.err.println("I/O error for " + path + ":\n" + e.toString());
+            System.exit(3);
+         }
+      }
+   }
+}
