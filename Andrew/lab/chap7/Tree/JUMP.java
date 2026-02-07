f837bbfccaa4123b3d3cb4b649381246bc7d678d
Kwanghoon Choi <lazyswamp@gmail.com>
2026-02-07 16:52:03 +0900

    chap7 트리

--- Andrew/lab/chap7/Tree/JUMP.java
+++ Andrew/lab/chap7/Tree/JUMP.java
@@ -1,12 +1,13 @@
 package Tree;
 import Temp.Temp;
 import Temp.Label;
+import Temp.LabelList;
 public class JUMP extends Stm {
   public Exp exp;
-  public Temp.LabelList targets;
-  public JUMP(Exp e, Temp.LabelList t) {exp=e; targets=t;}
-  public JUMP(Temp.Label target) {
-      this(new NAME(target), new Temp.LabelList(target,null));
+  public LabelList targets;
+  public JUMP(Exp e, LabelList t) {exp=e; targets=t;}
+  public JUMP(Label target) {
+      this(new NAME(target), new LabelList(target,null));
   }
   public ExpList kids() {return new ExpList(exp,null);}
   public Stm build(ExpList kids) {
