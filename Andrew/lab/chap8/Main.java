fc8b43edf2d6f3ab3d13d9ce91d101a5741427bd
Kwanghoon Choi <lazyswamp@gmail.com>
2026-02-07 23:05:01 +0900

    Main에 Linearlize-> Basic Block -> Trace 추가; Canon 폴더의 불필요한 Java 파일 삭제

--- Andrew/lab/chap8/Main.java
+++ Andrew/lab/chap8/Main.java
@@ -2,6 +2,10 @@ import syntaxtree.*;
 import visitor.*;
 import Translate.IRTranslator;
 import Tree.Print;
+import Tree.StmList;
+import Canon.Canon;
+import Canon.BasicBlocks;
+import Canon.TraceSchedule;
 
 public class Main {
    public static void main(String [] args) {
@@ -13,10 +17,28 @@ public class Main {
             root.accept(new PrettyPrintVisitor());
             TypeCheckVisitor tc = new TypeCheckVisitor();
             tc.check(root);
-            // Translate to IR and print
-            IRTranslator tr = new IRTranslator();
-            Tree.Stm ir = tr.translate(root);
-            new Print(System.out).prStm(ir);
+                  // Translate to IR
+                  IRTranslator tr = new IRTranslator();
+                  Tree.Stm ir = tr.translate(root);
+
+                  Print printer = new Print(System.out);
+                  System.out.println("=== IR (unscheduled) ===");
+                  printer.prStm(ir);
+
+                  // Canonicalize and linearize
+                  StmList linear = Canon.linearize(ir);
+                  System.out.println("=== Canonical linearized ===");
+                  for (StmList l = linear; l != null; l = l.tail) {
+                     printer.prStm(l.head);
+                  }
+
+                  // Basic blocks and trace scheduling
+                  BasicBlocks blocks = new BasicBlocks(linear);
+                  TraceSchedule ts = new TraceSchedule(blocks);
+                  System.out.println("=== Trace scheduled ===");
+                  for (StmList l = ts.stms; l != null; l = l.tail) {
+                     printer.prStm(l.head);
+                  }
          } catch (ParseException e) {
             System.err.println("Parse error in " + path + ":\n" + e.toString());
             System.exit(2);
